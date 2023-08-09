c9f5696cc377c8263ee2399a6865e66f2466cb27
Steven Otieno <steveotianga8991@gmail.com>
2023-08-10 02:25:33 +0300

    changed the vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.OTF"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
